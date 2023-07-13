ac682c4dc94ed9712ae55af85cb4829351a9d035
S1NiT0 <s1nit0@mail.com>
2023-07-13 15:23:53 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: A C progam that prints with printf function.
- *     *
- *      * Return: Always 0 (Success)
- *      */
+ * main - Entry point
+ *
+ * Description: A C progam that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+ **/
 
 int main(void)
 {
