af5d91c5dad0189620c3e6f506ee78675c703cc9
S1NiT0 <s1nit0@mail.com>
2023-07-13 15:19:07 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include<unistd.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: print a qoute using write function
- *     *    ssize_t write(int fd, const void *buf, size_t count);
- *      *
- *       * Return: 1 (error).
- *       */
+ * main - Entry point
+ *
+ * Description: print a qoute using write function
+ * ssize_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: 1 (error).
+ */
 
 
 int main(void)
