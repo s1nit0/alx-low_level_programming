ea09d1726b9ed0b05afa7eda13437c097cbc1309
S1NiT0 <s1nit0@mail.com>
2023-07-13 15:22:31 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: A C program that prints with put function
- *     *
- *      * Return: Alawys 0 (Success)
- *      */
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
+ **/
 
 int main(void)
 {
