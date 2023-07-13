6646f5b645af7e75098c7ce12e92b9f4e719cb05
S1NiT0 <s1nit0@mail.com>
2023-07-13 15:24:49 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: using sizeof to print the size of various types.
- *     *
- *      * Return: Always 0 (Success)
- *      */
+ * main - Entry point
+ *
+ * Description: using sizeof to print the size of various types.
+ *
+ * Return: Always 0 (Success)
+ **/
 
 int main(void)
 {
