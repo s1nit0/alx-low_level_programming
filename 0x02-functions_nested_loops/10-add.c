a7c0bd31ea391e44c6c15f1e2d39a88fd150b81e
S1NiT0 <s1nit0@mail.com>
2023-07-18 18:39:59 -0700

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * add -add two integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: sum of a & b
+ */
+
+int add(int a, int b)
+{
+	int c;
+	c = a + b;
+
+	return (c);
+}
