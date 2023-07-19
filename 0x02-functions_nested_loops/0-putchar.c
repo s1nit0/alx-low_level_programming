254f1cfc839c80861bce944ded361e08df5dae01
S1NiT0 <s1nit0@mail.com>
2023-07-18 18:30:30 -0700

    One

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,36 @@
 #include "main.h"
 
-/*
- * main print _putchar
+/**
+ * main - print _putchar
+ *
+ * is written by mbah
+ *
  * Return: 0
- */
+ **/
 
-int _putchar(char c)
+
+int main(void)
+
 {
-	return (write(1, &c, 1));
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+	return (0);
+
 }
