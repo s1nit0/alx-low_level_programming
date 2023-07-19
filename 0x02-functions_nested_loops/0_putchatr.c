3ebf6158fd568ebcf86e9f7668945f93bebe002e
S1NiT0 <s1nit0@mail.com>
2023-07-18 17:56:16 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0_putchatr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  * main - print _putchar
+ *   *
+ *    * is written by mbah
+ *     *
+ *      * Return: 0 (Success)
+ *      */
+
+int main(void)
+{
+	        char str[] = "_putchar";
+		        int ch;
+
+			        for (ch = 0; < 8; ch++)
+					                _putchar(str[ch]);
+				        _putchar('\n');
+
+					        return (0);
+
+}
+~                    
