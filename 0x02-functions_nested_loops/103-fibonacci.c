56e8a1426805aed108fa0d7364a7cffa60f93c7e
S1NiT0 <s1nit0@mail.com>
2023-07-18 20:07:44 -0700

    Fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -4,7 +4,7 @@
  * main - Entry point
  *
  * Description: Prints the sum of even-valued
- * 		Fibonacci sequence not exceed
+ *		Fibonacci sequence not exceed
  *		4million
  *
  * Return: Always 0 (Success)
