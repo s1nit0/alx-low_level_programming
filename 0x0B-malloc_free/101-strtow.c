c3ca4044c0858e538e13aa68a0971985e96ae027
S1NiT0 <s1nit0@mail.com>
2023-08-07 15:49:54 -0700

    Strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,51 +1,75 @@
 #include "main.h"
 
 /**
- * _strlen - find length of a string
- * @s: string
- * Return: int
+ * wrdcnt - counts the number of words in a string
+ * @s: string to count
+ *
+ * Return: int of number of words
  */
-
-
-int _strlen(char *s)
+int wrdcnt(char *s)
 {
-	int size = 0;
+	int i, n = 0;
 
-	for (; s[size] != '\0'; size++)
-	;
-	return (size);
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == ' ')
+		{
+			if (s[i + 1] != ' ' && s[i + 1] != '\0')
+				n++;
+		}
+		else if (i == 0)
+			n++;
+	}
+	n++;
+	return (n);
 }
 
 /**
- * *argstostr - description
- * @ac: int
- * @av: arguments
- * Return: string
+ * strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings
  */
-
-char *argstostr(int ac, char **av)
+char **strtow(char *str)
 {
-	int i = 0, nc = 0, j = 0, cmpt = 0;
-	char *s;
+	int i, j, k, l, n = 0, wc = 0;
+	char **w;
 
-	if (ac == 0 || av == NULL)
+	if (str == NULL || *str == '\0')
 		return (NULL);
-
-	for (; i < ac; i++, nc++)
-		nc += _strlen(av[i]);
-
-	s = malloc(sizeof(char) * nc + 1);
-	if (s == 0)
+	n = wrdcnt(str);
+	if (n == 1)
 		return (NULL);
-
-	for (i = 0; i < ac; i++)
+	w = (char **)malloc(n * sizeof(char *));
+	if (w == NULL)
+		return (NULL);
+	w[n - 1] = NULL;
+	i = 0;
+	while (str[i])
 	{
-		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
-			s[cmpt] = av[i][j];
-
-		s[cmpt] = '\n';
-		cmpt++;
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{
+			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+				;
+			j++;
+			w[wc] = (char *)malloc(j * sizeof(char));
+			j--;
+			if (w[wc] == NULL)
+			{
+				for (k = 0; k < wc; k++)
+					free(w[k]);
+				free(w[n - 1]);
+				free(w);
+				return (NULL);
+			}
+			for (l = 0; l < j; l++)
+				w[wc][l] = str[i + l];
+			w[wc][l] = '\0';
+			wc++;
+			i += j;
+		}
+		else
+			i++;
 	}
-	s[cmpt] = '\0';
-	return (s);
+	return (w);
 }
