c9a3347be76054279e892f8c27456e9822da892f
S1NiT0 <s1nit0@mail.com>
2023-11-09 13:28:49 +0000

    done

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,26 @@
+nclude "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint - add a new node at the beginning of the linked list
+ * @head: double pointer to the beginning of the linked list
+ * @n: the value added to the new node
+ * Return: a pointer to the new node or a NULL on failure
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	*head = new;
+	if (new->next != NULL)
+		(new->next)->prev = new;
+	return (new);
+}
---
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -0,0 +1,10 @@
+spalindrome(n):
+    string = str(n)
+    return string == string[::-1]
+
+array = []
+for i in range(999, 99, -1):
+    for j in range(i, 99, -1):
+        if ispalindrome(i * j):
+            array.append(i * j)
+print(max(array))
